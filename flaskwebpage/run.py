09e8e89ae0229c7bc5698b56faa3355339aaa542
terryyoung22 <38081981+terryyoung22@users.noreply.github.com>
2019-09-02 19:19:22 -0400

    changed 'app.py' section

--- flaskwebpage/run.py
+++ flaskwebpage/run.py
@@ -17,6 +17,7 @@ def do_something():
     return msg
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host='0.0.0.0')
+    # app.run(debug=True)
 
 #https://recycledrobot.co.uk/words/?flask <--the website
